af4e0fcccf0d899e411acdd83e2ca1c850f7aaa7
Ruturaj Deshmukh <ruturajd9696@gmail.com>
2024-10-07 22:55:30 +0530

    created the call with unique call-Id

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,13 +3,68 @@ import React, { useState } from "react";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
 import MeetingModal from "./MeetingModal";
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useToast } from "@/hooks/use-toast";
 
 const MeetingTypeList = () => {
+  const { toast } = useToast();
   const router = useRouter();
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >(undefined);
-  const createMeeting = () => {};
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+  const [value, setvalue] = useState({
+    dateTime: new Date(),
+    description: "",
+    link: "",
+  });
+  const [callDetails, setCallDetails] = useState<Call>();
+
+  //Here we are creating the new meeting
+  const createMeeting = async () => {
+    if (!user || !client) {
+      return;
+    }
+    try {
+      if (!value.dateTime) {
+        toast({
+          title: "Please select data and time",
+        });
+        return;
+      }
+      //Generate random-id
+      const id = crypto.randomUUID();
+      const call = client.call("default", id);
+      if (!call) {
+        throw new Error("Failed to create call ! ");
+      }
+      const startsAt =
+        value.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = value.description || "Instant Meeting";
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          },
+        },
+      });
+      setCallDetails(call);
+      if (!value.description) {
+        router.push(`/meeting/${call.id}`);
+      }
+      toast({
+        title: "Meeting Created Successfully !",
+      });
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Failed to create meeting !",
+      });
+    }
+  };
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
