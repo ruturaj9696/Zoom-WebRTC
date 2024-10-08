d9fc60d13231ff08f5b3590f2475436da7799815
Ruturaj Deshmukh <ruturajd9696@gmail.com>
2024-10-09 00:07:54 +0530

    meeting set-up completed

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,3 +1,35 @@
-export default function Page({ params }: { params: { id: string } }) {
-  return <div>Meeting Room : {params.id}</div>;
+"use client";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
+//It is client side component because it uses hooks like useUser() which is 3rd party hooks
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { Loader } from "lucide-react";
+import { useState } from "react";
+
+export default function Page({ params: { id } }: { params: { id: string } }) {
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+  const { call, isCallLoading } = useGetCallById(id);
+
+  if (!isLoaded || isCallLoading)
+    return (
+      <div className="w-full h-screen flex justify-center items-center text-white">
+        <Loader />
+      </div>
+    );
+  return (
+    <main className="w-full h-screen bg-red-50">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 }
---
+++ components/MeetingSetup.tsx
@@ -0,0 +1,57 @@
+"use client";
+import {
+  DeviceSettings,
+  useCall,
+  VideoPreview,
+} from "@stream-io/video-react-sdk";
+import React, { useEffect, useState } from "react";
+
+const MeetingSetup = ({
+  setIsSetupComplete,
+}: {
+  setIsSetupComplete(val: boolean): () => void;
+}) => {
+  const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false);
+  const call = useCall();
+  if (!call) {
+    throw new Error("useCall must be used in streamCall component");
+  }
+  useEffect(() => {
+    if (isMicCamToggledOn) {
+      call?.camera.disable();
+      call?.microphone.disable();
+    } else {
+      call?.camera.enable();
+      call?.microphone.enable();
+    }
+  }, [isMicCamToggledOn, call?.camera, call?.microphone]);
+  return (
+    <div className="flex w-full h-screen flex-col bg-dark-1 text-white items-center justify-center">
+      <h1 className="text-2xl font-bold">Setup</h1>
+      <VideoPreview />
+      <div className="flex h-16 justify-center items-center gap-3">
+        <label className=" flex justify-center items-center gap-2 font-medium">
+          <input
+            type="checkbox"
+            checked={isMicCamToggledOn}
+            onChange={(e) => setIsMicCamToggledOn(e.target.checked)}
+          />
+          Join with mic and camera off
+        </label>
+        {/* will open the device settings */}
+        <DeviceSettings />
+      </div>
+      <button
+        className="rounded-md bg-green-500 px-5 py-2.5"
+        onClick={() => {
+          call.join();
+          setIsSetupComplete(true);
+        }}
+      >
+        Join Meeitng
+      </button>
+    </div>
+  );
+};
+
+export default MeetingSetup;
