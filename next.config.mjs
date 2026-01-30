38ff2bcc35651795a2eb051c6cd78a2ed9fce292
Ruturaj Deshmukh <ruturajd9696@gmail.com>
2026-01-30 15:31:50 +0530

    fix:vercel deployment fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
+  },
+  // This can help with Vercel deployment issues
+  outputFileTracing: true,
+};
 
 export default nextConfig;
